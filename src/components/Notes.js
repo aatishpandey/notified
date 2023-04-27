5528f3efadafc1ba743c79b6e0627b8e66c22070
Aatish Pandey <32216307+aatishpandey@users.noreply.github.com>
2023-04-27 16:03:13 +0530

    added rich text editor,add note and display note functionality.

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,29 +1,90 @@
-import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useContext } from "react";
+import userContext from "../utils/userContext";
+import { getUserNotes, addUserNotes } from "../utils/useNotes.js";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import parse from "html-react-parser";
+
+const modules = {
+  toolbar: [
+    [{ font: [] }],
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ color: [] }, { background: [] }],
+    [{ script: "sub" }, { script: "super" }],
+    ["blockquote", "code-block"],
+    [{ list: "ordered" }, { list: "bullet" }],
+    ["link", "image", "video"],
+    ["bold", "italic", "underline", "strike"],
+    ["clean"],
+  ],
+};
 
 const Notes = () => {
-  const getUserNotes = async () => {
-    const encodedToken = localStorage.getItem("token");
-    try {
-      const res = await axios.get("api/user", {
-        headers: {
-          authorization: encodedToken,
-        },
-      });
-      console.log(res.data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
+  const { user, setUser } = useContext(userContext);
+  const [notes, setNotes] = useState([]);
+  const [noteInput, setNoteInput] = useState("");
+  const [note, setNote] = useState({
+    id: "",
+    content: "",
+  });
+  // console.log(noteInput);
   useEffect(() => {
-    getUserNotes();
+    setTimeout(() => getUserNotes(setNotes), 1000);
   }, []);
 
+  if (!user.isLoggedIn) return <h1>You are not logged in</h1>;
   return (
-    <>
-      <h1>Notes</h1>
-    </>
+    <div className="flex flex-col items-center">
+      <div className="note-input flex justify-around items-center m-4">
+        {/* <input
+          type="text"
+          className="border-2 border-slate-500"
+          placeholder="title"
+          value={note.title}
+          onChange={(e) => setNote({ ...note, title: e.target.value })}
+        />
+        <input
+          type="text"
+          className="border-2 border-slate-500"
+          placeholder="body"
+          value={note.body}
+          onChange={(e) => setNote({ ...note, body: e.target.value })}
+        /> */}
+
+        <ReactQuill
+          className="shadow-md"
+          theme="snow"
+          modules={modules}
+          placeholder="Write Something"
+          onChange={setNoteInput}
+        />
+      </div>
+
+      <button
+        className="bg-teal-600 font-bold text-white self-center py-1 px-2 rounded-md my-4"
+        onClick={() => addUserNotes(setNotes, { ...note, content: noteInput })}
+      >
+        Add Note
+      </button>
+      {notes.length === 0 ? (
+        <h1>"No Notes to Display"</h1>
+      ) : (
+        <div className="flex flex-col items-center border-2 m-4 p-4">
+          {notes.map((note, index) => {
+            return (
+              <div className="m-2 border-2 border-black p-4 w-full" key={index}>
+                <h1>
+                  {note.content.length === 0
+                    ? "Empty Note"
+                    : parse(note.content)}
+                </h1>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
   );
 };
 
---
+++ src/utils/useNotes.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+export const getUserNotes = async (setNotes) => {
+  const encodedToken = localStorage.getItem("token");
+  try {
+    const res = await axios.get("api/notes", {
+      headers: {
+        authorization: encodedToken,
+      },
+    });
+    console.log(res);
+    if (res.status === 200) {
+      setNotes([...res.data.notes]);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const addUserNotes = async (setNotes, note) => {
+  const encodedToken = localStorage.getItem("token");
+  try {
+    const res = await axios.post(
+      "/api/notes",
+      { note },
+      {
+        headers: {
+          authorization: encodedToken,
+        },
+      }
+    );
+    if (res.status === 201) setNotes([...res.data.notes]);
+    console.log(res);
+  } catch (err) {
+    console.log(err);
+  }
+};
